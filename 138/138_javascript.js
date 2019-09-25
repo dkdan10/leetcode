cbfbdb4b7d30283d824dbbc06a9252085de6fe55
Daniel Keinan <danielkeinan0@gmail.com>
2019-09-25 17:38:30 -0400

    add leetcode 138

---
+++ 138/138_javascript.js
@@ -0,0 +1,47 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,next,random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ */
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+
+var copyRandomList = function (head) {
+    if (!head) return head
+    const newHead = new Node(head.val)
+    const old2New = { [head.val]: newHead }
+    const q = [head]
+    while (q.length) {
+        const node = q.shift()
+        let dupedNode;
+        if (old2New[node.val]) {
+            dupedNode = old2New[node.val]
+        } else {
+            dupedNode = new Node(node.val)
+            old2New[node.val] = dupedNode
+        }
+        if (node.next) {
+            if (old2New[node.next.val]) {
+                dupedNode.next = old2New[node.next.val]
+            } else {
+                dupedNode.next = new Node(node.next.val)
+            }
+            old2New[node.next.val] = dupedNode.next
+            q.push(node.next)
+        }
+        if (node.random) {
+            if (old2New[node.random.val]) {
+                dupedNode.random = old2New[node.random.val]
+            } else {
+                dupedNode.random = new Node(node.random.val)
+                old2New[node.random.val] = dupedNode.random
+            }
+        }
+    }
+    return newHead
+};
