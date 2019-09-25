e7341f59833ee1d56d09ad780a3d702d2c386f71
Daniel Keinan <danielkeinan0@gmail.com>
2019-09-25 18:50:25 -0400

    add 138

--- 138/138_javascript.js
+++ 138/138_javascript.js
@@ -11,6 +11,38 @@
  * @return {Node}
  */
 
+//  WITH FACTORED OUT DUPLICATION METHOD
+var copyRandomList = function (head) {
+    if (!head) return head
+    const newHead = new Node(head.val)
+    const old2New = { [head.val]: newHead }
+    const q = [head]
+    while (q.length) {
+        const node = q.shift()
+        let dupedNode = dupeNode(node, old2New)
+        if (node.next) {
+            dupedNode.next = dupeNode(node.next, old2New)
+            q.push(node.next)
+        }
+        if (node.random) {
+            dupedNode.random = dupeNode(node.random, old2New)
+        }
+    }
+    return newHead
+};
+
+const dupeNode = (node, dictionary) => {
+    if (dictionary[node.val]) {
+        return dictionary[node.val]
+    } else {
+        const coppiedNode = new Node(node.val)
+        dictionary[coppiedNode.val] = coppiedNode
+        return coppiedNode
+    }
+}
+
+
+//  FIRST
 var copyRandomList = function (head) {
     if (!head) return head
     const newHead = new Node(head.val)
@@ -25,13 +57,14 @@ var copyRandomList = function (head) {
             dupedNode = new Node(node.val)
             old2New[node.val] = dupedNode
         }
+
         if (node.next) {
             if (old2New[node.next.val]) {
                 dupedNode.next = old2New[node.next.val]
             } else {
                 dupedNode.next = new Node(node.next.val)
+                old2New[node.next.val] = dupedNode.next
             }
-            old2New[node.next.val] = dupedNode.next
             q.push(node.next)
         }
         if (node.random) {
