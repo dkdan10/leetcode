cc72d5aeceb75e6c6d6fd48cb0b5cc471a6cb435
Daniel Keinan <danielkeinan0@gmail.com>
2019-09-25 16:34:54 -0400

    add more solutions to 3

--- 3/3_javascript.js
+++ 3/3_javascript.js
@@ -2,6 +2,33 @@
  * @param {string} s
  * @return {number}
  */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+// SECOND ATTEMPT
+var lengthOfLongestSubstring = function (s) {
+
+    let longLength = 0
+    let currentLength = 0
+    const lastIndexChar = {}
+    for (let i = 0; i < s.length; i++) {
+        const currentChar = s.charAt(i)
+        if (lastIndexChar[currentChar] === undefined || lastIndexChar[currentChar] < i - currentLength) {
+            currentLength++
+        } else if (lastIndexChar[currentChar] > i - currentLength) {
+            currentLength = (i - lastIndexChar[currentChar])
+        }
+        
+        lastIndexChar[currentChar] = i
+        longLength = Math.max(currentLength, longLength)
+    }
+
+    return longLength
+};
+
+// ONE ATTEMPT
 var lengthOfLongestSubstring = function(s) {
     let longestSub = 0
     let subStartIdx = 0
