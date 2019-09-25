f8f3f8906f69ba85624f0c77b072778ccc8aead8
Daniel Keinan <danielkeinan0@gmail.com>
2019-09-25 13:36:19 -0400

    add leetcode 98

---
+++ 98/98_javascript.js
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+// ITERATIVELY
+var isValidBST = function (root) {
+    if (root === null) return true
+    const stack = [{ node: root, max: Infinity, min: -Infinity }]
+    while (stack.length) {
+        const { node, max, min } = stack.shift()
+        if (node.left) stack.push({ node: node.left, max: node.val, min: min })
+        if (node.right) stack.push({ node: node.right, max: max, min: node.val })
+        if (node.val <= min) return false
+        if (node.val >= max) return false
+    }
+    return true
+};
+
+//  RECURSIVELY
+var isValidBST = function (root) {
+    const isValidRoot = (root, min, max) => {
+        if (root === null) return true
+        if (!root.left && !root.right) {
+            if (root.val > min && root.val < max) {
+                return true
+            } else {
+                return false
+            }
+        }
+        if (root.left) {
+            if (!isValidRoot(root.left, min, root.val) || !(root.val > root.left.val)) {
+                return false
+            }
+        }
+        if (root.right) {
+            if (!isValidRoot(root.right, root.val, max) || !(root.val < root.right.val)) {
+                return false
+            }
+        }
+
+        return true
+    }
+    return isValidRoot(root, -Infinity, Infinity)
+};
